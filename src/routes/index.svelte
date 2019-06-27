2445ea9466f23c854fc8c4266f100684ce556f08
Maxim Matyunin <matyunya@gmail.com>
2019-06-27 17:00:51 +0900

    add readme

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,12 @@
+<h2>Hi</h2>
+<p>
+  Smelte is <a class="a" href="https://vuetifyjs.com">Vuetify</a> inspired collection
+  of <a class="a" href="https://material.io/">Material design</a> components for Svelte
+  built with <a class="a" href="https://tailwindcss.com">Tailwind CSS</a>.
+  There's only a handful of components at the moment but this project is aimed to grow into
+  a powerful UI framework like Vuetify but keeping minimal bundle size thanks to Svelte.
+</p>
 
-	
+<p>There's no proper distribution at the moment rather than a Sapper app <a href="https://github.com/matyunya/smelte">repo</a> containing the site you're looking at right now.
+It is still very much proof of concept stage, as well as author's ongoing attempt at learning Svelte
+but please feel free to contribute or drop me a line at <a href="mailto:matyunya@gmail.com">matyunya@gmail.com</a>.</p>
