6a33eb284d622c0e71e6505623f23fe7b3648c66
Maxim Matyunin <matyunya@gmail.com>
2019-06-28 14:17:45 +0900

    more better chips

--- src/components/Chip/Chip.svelte
+++ src/components/Chip/Chip.svelte
@@ -6,6 +6,9 @@
 
   export let removable;
   export let icon;
+  export let outlined = false;
+  export let selected = false;
+  export let selectable = true;
 
   let value = true;
 
@@ -16,31 +19,69 @@
     value = false;
   }
 
+  function select() {
+    if (!selectable) return;
+
+    selected = true;
+  }
+
 </script>
 
 <style>
 .p-1\/2 {
   padding: 0.125rem;
 }
+
+.outlined {
+  @apply bg-transparent border border-gray-400 border-solid;
+
+  &:hover {
+    @apply bg-gray-50;
+  }
+}
+
+.selected {
+  @apply border-primary-500 text-primary-500 bg-primary-100;
+
+  &:hover {
+    @apply bg-primary-50;
+  }
+}
 </style>
 
 {#if value}
-  <div
+  <span
+    class="mx-1 inline-block"
     out:fade={{ duration: 100 }}>
-    <button class="rounded-full bg-gray-300 px-2 py-1" on:click>
+    <button
+      class="flex items-center rounded-full bg-gray-300 pr-2 pl-1 py-1"
+      on:click
+      class:outlined
+      class:selected
+      class:ripple-primary={selectable}
+      on:click={select}
+    >
       {#if icon}
-        <Icon small>{icon}</Icon>
+        <Icon
+          small
+          color={selected ? 'text-primary-400' : 'text-gray-400'}
+          >{icon}</Icon>
       {/if}
-      <span class="px-2">
+      <span class="px-3">
         <slot></slot>
       </span>
       {#if removable}
         <span
-          class="rounded-full bg-gray-700 p-1/2 hover:bg-gray-500 inline-flex items-center cursor-pointer"
+          class="rounded-full p-1/2 hover:bg-gray-500 inline-flex items-center cursor-pointer"
+          class:bg-gray-600={!selected}
+          class:bg-primary-400={selected}
           on:click|stopPropagation={close}>
-          <Icon color="text-gray-300" xs>close</Icon>
+          <Icon
+            color={selected ? 'text-white' : 'text-gray-300'}
+            xs
+          >close</Icon>
         </span>
       {/if}
     </button>
-  </div>
+  </span>
 {/if}
--- src/routes/components/chips.svelte
+++ src/routes/components/chips.svelte
@@ -1,7 +1,69 @@
 <script>
 	import Chip from 'components/Chip';
+	import Snackbar from 'components/Snackbar';
+	import Button from 'components/Button';
+
+  let closed = false;
+  let clicked = false;
 </script>
 
 <h4 class="pb-8">Chips</h4>
 
-<Chip removable>test</Chip>
+<h5 class="mt-6 mb-2">Basic</h5>
+<Chip
+  icon="face"
+  removable
+  selectable
+  on:close={() => closed = true}
+  on:click={() => clicked = true}
+>test</Chip>
+
+<h5 class="mt-6 mb-2">Outlined</h5>
+<Chip
+  icon="pan_tool"
+  outlined
+  removable
+  selectable
+  on:close={() => closed = true}
+  on:click={() => clicked = true}
+>Cats</Chip>
+<Chip
+  icon="print"
+  outlined
+  removable
+  selectable
+  on:close={() => closed = true}
+  on:click={() => clicked = true}
+>Dogs</Chip>
+<Chip
+  icon="pageview"
+  outlined
+  removable
+  selectable
+  on:close={() => closed = true}
+  on:click={() => clicked = true}
+>Plants</Chip>
+<Chip
+  icon="pets"
+  outlined
+  removable
+  selectable
+  on:close={() => closed = true}
+  on:click={() => clicked = true}
+>Parents</Chip>
+
+<Snackbar
+  bind:value={closed}
+>Chip was removed successfully.
+  <div slot="action">
+    <Button text on:click={() => closed = false}>Dismiss</Button>
+  </div>
+</Snackbar>
+
+<Snackbar
+  bind:value={clicked}
+>Chip was clicked successfully.
+  <div slot="action">
+    <Button text on:click={() => clicked = false}>Dismiss</Button>
+  </div>
+</Snackbar>
