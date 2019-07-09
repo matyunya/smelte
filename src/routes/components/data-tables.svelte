980187491d63c03443d921fd86f2633585fdfd79
Maxim Matyunin <matyunya@gmail.com>
2019-07-09 17:01:30 +0900

    formatting

--- src/routes/components/data-tables.svelte
+++ src/routes/components/data-tables.svelte
@@ -1,30 +1,29 @@
 <script>
-  import DataTable from 'components/DataTable';
+  import DataTable from "components/DataTable";
 
-  import data from './data.json';
+  import data from "./data.json";
 </script>
 
 <DataTable
   data={data._embedded.episodes}
   columns={[
-    { label: "ID", field: "id", class: 'w-10', },
+    { label: "ID", field: "id", class: "w-10", },
     {
       label: "Season/Episode",
-      field: "season/episode",
       value: (v) => `S${v.season}E${v.number}`,
-      class: 'w-10'
+      class: "w-10"
     },
     { field: "name", class: "w-10" },
     {
       field: "summary",
-      value: v => v && v.summary ? v.summary.split('.')[0] + "." : '',
+      value: v => v && v.summary ? v.summary.split(".")[0] + "." : "",
       class: "text-xs text-gray-700 w-64 caption text-left" },
     {
       field: "thumbnail",
       value: (v) => v && v.image
-        ? `<img src="${v.image.medium}" height="70" alt="${v.name}">`
-        : '',
-      class: 'w-50',
+        ? `<img src="${v.image.medium.replace("http", "https")}" height="70" alt="${v.name}">`
+        : "",
+      class: "w-50",
       sortable: false,
     }
   ]}
