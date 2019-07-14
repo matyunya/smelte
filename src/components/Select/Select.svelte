911ae3561211a15fa3bbbb94dcff3843fd2293de
Maxim Matyunin <matyunya@gmail.com>
2019-07-15 00:27:41 +0900

    fix selected label

--- src/components/Select/Select.svelte
+++ src/components/Select/Select.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { createEventDispatcher } from "svelte";
+  import { createEventDispatcher, onMount } from "svelte";
   import { fly } from "svelte/transition";
   import { quadOut, quadIn } from "svelte/easing";
   import List from "../List/List.svelte";
@@ -30,6 +30,7 @@
   let showList = false;
   let filteredItems = items;
   let itemsProcessed = [];
+  let selectedLabel = '';
 
   const props = {
     outlined,
@@ -48,9 +49,17 @@
     appendBaseClasses,
   };
 
-  $: itemsProcessed = items.map(i => typeof i !== 'object'
+  function process(it) {
+    return it.map(i => typeof i !== 'object'
      ? ({ value: i, text: i })
      : i);
+  }
+
+  $: itemsProcessed = process(items);
+  
+  onMount(() => {
+    selectedLabel = getLabel(value);
+  })
 
   const inProps = { y: 10, duration: 50, easing: quadIn };
   const outProps = { y: -10, duration: 100, easing: quadOut, delay: 50 };
@@ -59,8 +68,6 @@
   function getLabel(value) {
     return value ? (itemsProcessed.find(i => i.value === value) || {}).text : "";
   }
-
-  let selectedLabel = getLabel(value);
 
   function filterItems({ target }) {
     filteredItems = itemsProcessed.filter(i =>
