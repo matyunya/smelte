1b24d44c72118e6a5632e593a825e43947eca989
Maxim Matyunin <matyunya@gmail.com>
2019-07-16 20:22:09 +0900

    add container to the table

--- src/routes/components/data-tables.svelte
+++ src/routes/components/data-tables.svelte
@@ -19,41 +19,43 @@
   getData();
 </script>
 
-<DataTable
-  {data}
-  {loading}
-  on:update={({ detail }) => {
-    const { column, item, value } = detail;
+<div class="overflow-auto p-1">
+  <DataTable
+    {data}
+    {loading}
+    on:update={({ detail }) => {
+      const { column, item, value } = detail;
 
-    const index = data.findIndex(i => i.id === item.id);
+      const index = data.findIndex(i => i.id === item.id);
 
-    data[index][column.field] = value;
-  }}
-  columns={[
-    { label: "ID", field: "id", class: "md:w-10", },
-    {
-      label: "Ep.",
-      value: (v) => `S${v.season}E${v.number}`,
-      class: "md:w-10",
-      editable: false,
-    },
-    { field: "name", class: "md:w-10" },
-    {
-      field: "summary",
-      textarea: true,
-      value: v => v && v.summary ? v.summary : "",
-      class: "text-sm text-gray-700 caption w-full"
-    },
-    {
-      field: "thumbnail",
-      value: (v) => v && v.image
-        ? `<img src="${v.image.medium.replace("http", "https")}" height="70" alt="${v.name}">`
-        : "",
-      class: "w-48",
-      sortable: false,
-      editable: false,
-    }
-  ]}
-/>
+      data[index][column.field] = value;
+    }}
+    columns={[
+      { label: "ID", field: "id", class: "md:w-10", },
+      {
+        label: "Ep.",
+        value: (v) => `S${v.season}E${v.number}`,
+        class: "md:w-10",
+        editable: false,
+      },
+      { field: "name", class: "md:w-10" },
+      {
+        field: "summary",
+        textarea: true,
+        value: v => v && v.summary ? v.summary : "",
+        class: "text-sm text-gray-700 caption md:w-full sm:w-64"
+      },
+      {
+        field: "thumbnail",
+        value: (v) => v && v.image
+          ? `<img src="${v.image.medium.replace("http", "https")}" height="70" alt="${v.name}">`
+          : "",
+        class: "w-48",
+        sortable: false,
+        editable: false,
+      }
+    ]}
+  />
 
-<Code code={table} />
+  <Code code={table} />
+</div>
