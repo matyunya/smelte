3b1009de234ae9e48c0e664427ca8134e475079f
Maxim Matyunin <matyunya@gmail.com>
2019-07-30 14:58:12 +0900

    chip variant

--- src/components/Chip/variants.js
+++ src/components/Chip/variants.js
@@ -2,6 +2,7 @@ module.exports = {
   all: color => [
     `border-${color}-500`,
     `text-${color}-500`,
+    `hover:bg-${color}-50`,
     `bg-${color}-50`,
     `bg-${color}-100`,
     `bg-${color}-300`,
