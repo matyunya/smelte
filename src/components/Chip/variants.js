fe78dcdf1ace6491735e20c421a2076e672c90c8
Maxim Matyunin <matyunya@gmail.com>
2019-07-30 11:12:15 +0900

    variants

--- src/components/Chip/variants.js
+++ src/components/Chip/variants.js
@@ -2,6 +2,8 @@ module.exports = {
   all: color => [
     `border-${color}-500`,
     `text-${color}-500`,
+    `bg-${color}-50`,
+    `bg-${color}-100`,
     `bg-${color}-300`,
     `bg-${color}-400`,
     `text-${color}-400`
