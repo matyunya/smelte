50352cce250d69625f7d69ce3a5c11d8d149e7dc
Kiho <kihoc22@gmail.com>
2019-08-02 08:56:25 -0700

    Select - change div tag to input

--- src/components/TextField/TextField.svelte
+++ src/components/TextField/TextField.svelte
@@ -195,16 +195,16 @@
         on:blur={toggleFocused}
         placeholder={!value ? placeholder : ''} />
     {:else if select && !autocomplete}
-      <div
+      <input
+        readonly
         class="select {iClasses}"
         on:click={toggleFocused}
         on:change
         on:input
         on:click
         on:blur
-        on:focus>
-        {value}
-      </div>
+        on:focus
+        {value} />
     {/if}
 
     <div class={pClasses}>
