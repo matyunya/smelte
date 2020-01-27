271022c6349df92908dc24149d2beb59c0221b58
Maxim Matyunin <matyunya@gmail.com>
2020-01-27 13:32:37 +0900

    lint

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,17 @@
 <h2>Hi</h2>
 <p class="pb-4">
-  Smelte is a collection of <a class="a" href="https://material.io/">Material design</a>
+  Smelte is a collection of
+  <a class="a" href="https://material.io/">Material design</a>
   components for Svelte built with
   <a class="a" href="https://tailwindcss.com">Tailwind CSS.</a>
-  The project was inspired by <a class="a" href="https://vuetifyjs.com">Vuetify</a>,
-  but comes at much lower price (all of this website is only a fraction of both JS (670 vs 50 Kb)
-  and CSS (110 vs ~10 Kb) payloads of even the most <a href="https://vuetifyjs.com/en/examples/layouts/baseline">
-  basic Vuetify example layout</a>), and of course has dark mode.
+  The project was inspired by
+  <a class="a" href="https://vuetifyjs.com">Vuetify</a>
+  , but comes at much lower price (all of this website is only a fraction of
+  both JS (670 vs 50 Kb) and CSS (110 vs ~10 Kb) payloads of even the most
+  <a href="https://vuetifyjs.com/en/examples/layouts/baseline">
+    basic Vuetify example layout
+  </a>
+  ), and of course has dark mode.
 </p>
 
 <p>
