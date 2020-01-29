4f16c1341ff013a53674fc049f0419fc49f134c7
Maxim Matyunin <matyunya@gmail.com>
2020-01-29 14:07:44 +0900

    dark mode tutorial

--- src/routes/dark-mode.svelte
+++ src/routes/dark-mode.svelte
@@ -1,5 +1,6 @@
 <script>
   import Code from "docs/Code.svelte";
+  import { darkMode } from "dark";
 </script>
 
 <h4 class="pb-8">Dark mode</h4>
@@ -8,11 +9,52 @@
   <a class="a" href="https://tailwindcss.com/docs/configuring-variants/">
     feature
   </a>
-  of Tailwind to enable dark mode.
+  of Tailwind to enable dark mode. Basic dark mode switch looks like this:
 </p>
+
+<Code code={`<script>
+  import dark from "smelte/src/dark";
+
+  const darkMode = dark();
+</script>
+
+<button bind:value={$darkMode}>Toggle dark mode</button>
+`}
+/>
+
+<p>
+  This will append <span class="code-inline">mode-dark</span> class to the document body
+  which will enable all generated classes preceded by pseudo-class "dark:".
+  By default smelte generates following variants:
+</p>
+
+<Code code={`
+backgroundColor: ["dark", "dark-hover", "hover"],
+borderColor: ["dark", "dark-focus"],
+textColor: ["dark", "dark-hover", "dark-active"]
+`}
+/>
+
+<p>
+Now you can use dark theme classes like <span class="code-inline">dark:bg-white</span>
+(try using the theme toggle on the top right).
+</p>
+
+<div class="transition p-10 my-10 bg-black dark:bg-white text-white dark:text-black">
+  I am a {$darkMode ? "dark" : "light"} div.
+</div>
+
+<Code code={`
+<div class="transition p-10 my-10 bg-black dark:bg-white text-white dark:text-black">
+  I am a {$darkMode ? "dark" : "light"} div.
+</div>
+`}
+/>
+
+
 
 <p>
   If you don't need dark mode at all, you can pass
-  <span class="code-inline">dark-mode: false</span>
+  <span class="code-inline">darkMode: false</span>
   to the smelte-rollup-plugin and it will generate no extra CSS.
 </p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,10 +4,9 @@
 
 <h2>Hi</h2>
 <p class="pb-4">
-  Smelte is a collection of
-  <a class="a" href="https://material.io/">Material design</a>
-  components for Svelte built with
-  <a class="a" href="https://tailwindcss.com">Tailwind CSS.</a>
+  Smelte is a UI framework built on top of Svelte and <a class="a" href="https://tailwindcss.com">Tailwind CSS</a> using Material Design spec.
+  It comes with many components and utility functions making it easy to build beautiful responsive layouts while keeping
+  bundle size and performance at check all thanks to Svelte.
   The project was initially inspired by
   <a class="a" href="https://vuetifyjs.com">Vuetify</a>
   , but comes at much lower price. All of this website is only a fraction of
@@ -15,7 +14,7 @@
   <a class="a" href="https://vuetifyjs.com/en/examples/layouts/baseline">
     basic Vuetify example layout
   </a>
-  ), and of course has dark mode.
+  ), and of course has <a class="a" href="dark-mode">dark mode</a>.
 </p>
 
 <p class="pb-4">
@@ -48,10 +47,8 @@ plugins = [
     output: "public/global.css" // it defaults to static/global.css which is probably what you expect in Sapper
   }),
 ]`} />
-
 Then you should add Tailwind utilites CSS in your app component.
 <Code code={`import "smelte/src/tailwind.css";`} />
-
 You might also need to include material icons in your template's if you use any:
 <Code
   code={`<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">`} />
