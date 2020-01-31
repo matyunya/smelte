307bda16d74c6961c6598f722010b3f1492f234a
jacksloan <jbsloan10@gmail.com>
2020-01-31 10:39:07 -0600

    Sidenav cant be opened after leaving drawers demo

--- src/routes/components/_layout.svelte
+++ src/routes/components/_layout.svelte
@@ -1,6 +1,8 @@
 <script>
+  import { showNav } from "stores.js";
   export let segment = "";
 
+  showNav.set(true);
   $: n = (segment || "").replace(new RegExp('-', 'g'), ' ');
   $: name = n.length ? n.charAt(0).toUpperCase() + n.slice(1) : "";
 
