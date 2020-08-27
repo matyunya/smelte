8de750e5e6b5bca1937bd87b09e1e649ba2db32f
Michael Lucht <michael@pop-os.localdomain>
2020-08-27 16:45:47 +0200

    Add Types for List

---
+++ src/components/List/ListItem.d.ts
@@ -0,0 +1,74 @@
+import { SvelteTypedComponent , SvelteAllProps } from 'svelte-typed-component';
+export default class ListItem extends SvelteTypedComponent<ListItemProps, ListItemEvents, ListItemSlots> {
+    get item(): any;
+    get items(): any[];
+    get level(): any;
+}
+declare const _ListItemProps: {
+    /** Prepend item with icon.
+     * 
+     * Default: empty string
+     */
+    icon?: string;
+    /** Item id.
+     * 
+     * Default: empty string
+     */
+    id?: string;
+    /** Selected item value.
+     * 
+     * Default: empty string
+    */
+    value?: string;
+    /** Item text.
+     * 
+     * Default: empty string
+     */
+    text?: string;
+    /** Item subheading.
+     * 
+     * Default: empty string
+     */
+    subheading?: string;
+    /** Disabled state. Bindable.
+     * 
+     * Default: false
+     */
+    disabled?: boolean;
+    /** Dense variant.
+     * 
+     * Default: false
+     */
+    dense?: boolean;
+    /** Is selected. Bindable.
+     * 
+     * Default: false
+     */
+    selected?: boolean;
+    /** Tab index.
+     * 
+     * default: null
+     */
+    tabindex?: number|null;
+    /** Selected item classes */
+    selectedClasses?: string;
+    /** Subheading item classes */
+    subheadingClasses?: string;
+    /** href */
+    to?: string;
+    /** Item wrapper classes */
+    classes?: string;
+    /** --not implemented-- Is navigation item
+     */
+    navigation?:boolean
+} & SvelteAllProps;
+declare const _ListItemEvents: {
+    click: MouseEvent;
+};
+declare const _ListItemSlots: {
+    default: {};
+};
+export declare type ListItemProps = typeof _ListItemProps;
+export declare type ListItemEvents = typeof _ListItemEvents;
+export declare type ListItemSlots = typeof _ListItemSlots;
+export {};
