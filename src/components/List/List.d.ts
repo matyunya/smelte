70cc8316f8401e140c34747d8c45d7eee1a42282
micha-lmxt <github@lucht-familie.de>
2020-08-31 21:11:17 +0200

    Add types List

---
+++ src/components/List/List.d.ts
@@ -0,0 +1,48 @@
+import { SvelteTypedComponent , SvelteAllProps } from 'svelte-typed-component';
+import { ListItemBase } from './ListItem';
+
+
+export default class List extends SvelteTypedComponent<ListProps, ListEvents, ListSlots> {
+    get level(): any;
+    get text(): string;
+    get item(): {};
+    get to(): any;
+    get selectedClasses(): (i: string) => string;
+    get itemClasses(): (i: string) => string;
+}
+declare const _ListProps: {
+    items?: ListItemBase[];
+    /** Selected item value, Bindable.
+     * 
+     * Default: empty string
+     */
+    value?: string;
+    /** Dense variant.
+     * 
+     * Default: false
+     */
+    dense?: boolean;
+    /** Is dropdown select.
+     * 
+     * Default false
+     */
+    select?: boolean;
+    classes?: string;
+    /** -not implemented - Is navigation drawer list	 */
+    navigation?:boolean;
+} & SvelteAllProps;
+declare const _ListEvents: {
+    change: any;
+    click: any;
+};
+declare const _ListSlots: {
+    item: {
+        item: {};
+        dense: boolean;
+        value: string;
+    };
+};
+export declare type ListProps = typeof _ListProps;
+export declare type ListEvents = typeof _ListEvents;
+export declare type ListSlots = typeof _ListSlots;
+export {};
