ada9d5b07778a9afc28db5e1d4fcd84d9d05bda6
micha-lmxt <github@lucht-familie.de>
2020-08-31 21:26:05 +0200

    Add types src

---
+++ src/stores.d.ts
@@ -0,0 +1,5 @@
+export declare const right: import("svelte/store").Writable<boolean>;
+export declare const persistent: import("svelte/store").Writable<boolean>;
+export declare const elevation: import("svelte/store").Writable<boolean>;
+export declare const showNav: import("svelte/store").Writable<boolean>;
+export declare const dark: import("svelte/store").Writable<boolean>;
