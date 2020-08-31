6d825c7d458d07397cd13f6fb07b87dd8e129858
micha-lmxt <github@lucht-familie.de>
2020-08-31 21:17:06 +0200

    Add types Select

---
+++ src/components/Select/Select.d.ts
@@ -0,0 +1,126 @@
+import { SvelteTypedComponent , SvelteAllProps } from 'svelte-typed-component';
+/** Select components are used for collecting user provided information from a list of options.
+ * One may bind to a select via on:change event. 
+ * 
+ * Props: items, value, label, selectedLabel, color, outlined, placeholder, 
+ * hint, error, append, dense, persistentHint, noUnderline, showList, classes, 
+ * optionsClasses, inputWrapperClasses, apendClasses, labelClasses, inputClasses,
+ * prependClasses, listClasses, selectedClasses, itemClasses, add, remove, replace
+ * 
+ * Slots: select, options
+ * */
+export default class Select extends SvelteTypedComponent<SelectProps, SelectEvents, SelectSlots> {
+    get text(): string;
+}
+declare const _SelectProps: {
+    /** Array of Items
+     * 
+     * Default: []
+     */
+    items?: ({value:string, text: string}|string)[];
+    /** Selected Value. Bindable
+     * 
+     * Default: empty string
+     */
+    value?: string;
+    /** Label of the TextField
+     * 
+     * Default: empty string
+     */
+    label?: string;
+    /** Selected Label. Bindable.
+     * 
+     * Default: undefined
+     */
+    selectedLabel?: string;
+    /** Color variant, accepts any of the main colors described in Tailwind config. 
+     * 
+     * Default: "primary" */
+    color?: string;
+    /** Outlined variant.
+     * 
+     * Default: false
+     */
+    outlined?: boolean;
+    /** Placeholder Text.
+     * 
+     * Default: empty string
+     */
+    placeholder?: string;
+    /** Hint 
+     * 
+     * Default: empty string
+    */
+    hint?: string;
+    /** Error variant.
+     * 
+     * Default: false
+     */
+    error?: boolean;
+    /** Append Icon.
+     * 
+     * Default: "arrow_drop_down"
+     */
+    append?: string;
+    /** Dense variant.
+     * 
+     * Default: false
+     */
+    dense?: boolean;
+    /** Persisten Hint variant.
+     * 
+     * Default: false
+     */
+    persistentHint?: boolean;
+    /** Autocomplete variant.
+     * 
+     * Default: false
+     */
+    autocomplete?: boolean;
+    /** No underline variant.
+     * 
+     * Default: false
+     */
+    noUnderline?: boolean;
+    /** Show List. Bindable.
+     * 
+     * Default: false
+     */
+    showList?: boolean;
+    classes?: string;
+    optionsClasses?: string;
+    inputWrapperClasses?: (i: string) => string;
+    appendClasses?: (i: string) => string;
+    labelClasses?: (i: string) => string;
+    inputClasses?: (i: string) => string;
+    prependClasses?: (i: string) => string;
+    listClasses?: (i: string) => string;
+    selectedClasses?: (i: string) => string;
+    itemClasses?: (i: string) => string;
+    /** List of classes to remove from the component (blank space separated).
+     * 
+     * Default: empty string
+     */
+    remove?: string;
+    /** List of classes to add to the component (blank space separated).
+     * 
+     * Default: empty string
+     */
+    add?: string;
+    /** List of classes to replace in the component.
+     * 
+     * Default: {}
+     */
+    replace?: {[key:string]:string};
+} & SvelteAllProps;
+declare const _SelectEvents: {
+    click: any;
+};
+declare const _SelectSlots: {
+    select: {};
+    options: {};
+};
+export declare type SelectProps = typeof _SelectProps;
+export declare type SelectEvents = typeof _SelectEvents;
+export declare type SelectSlots = typeof _SelectSlots;
+export {};
---
+++ src/components/Select/index.d.ts
@@ -0,0 +1,5 @@
+import Select from "./Select";
+export default Select;
+export declare const variants: {
+    all: (color: string) => string[];
+};
