02498a7f4b4e006cfd6bb5a309094caaf2cdc2e8
micha-lmxt <github@lucht-familie.de>
2020-08-31 21:20:04 +0200

    Add types Tabs

---
+++ src/components/Tabs/Tab.d.ts
@@ -0,0 +1,31 @@
+import { SvelteTypedComponent } from 'svelte-typed-component';
+/** Tab.
+ * 
+ * Props: selecte, id
+ * 
+ * Slots: default
+ */
+export default class Tab extends SvelteTypedComponent<TabProps, TabEvents, TabSlots> {
+}
+declare const _TabProps: {
+    /** Selectd
+     * 
+     * Default: false
+     */
+    selected?: boolean;
+    /** ID
+     * 
+     * Default: null
+     */
+    id?: string|null;
+};
+declare const _TabEvents: {
+    [evt: string]: CustomEvent<any>;
+};
+declare const _TabSlots: {
+    default: {};
+};
+export declare type TabProps = typeof _TabProps;
+export declare type TabEvents = typeof _TabEvents;
+export declare type TabSlots = typeof _TabSlots;
+export {};
---
+++ src/components/Tabs/index.d.ts
@@ -0,0 +1,6 @@
+import TabButton from "./TabButton";
+import Tab from "./Tab";
+import Indicator from "./Indicator";
+import Tabs from "./Tabs";
+export default Tabs;
+export { TabButton, Tab, Tabs, Indicator };
